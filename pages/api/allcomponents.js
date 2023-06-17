6991dd0345bc6a7fb9033e34262f1625e4ffc1b9
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-06-17 18:46:57 +0530

    1 file modified

--- pages/api/allcomponents.js
+++ pages/api/allcomponents.js
@@ -24,5 +24,30 @@ const ALLCOMPONENTS = [
     title: 'Split View',
     type: 'splitview',
   },
+  {
+    id: 5,
+    title: 'Header',
+    type: 'header',
+  },
+  {
+    id: 6,
+    title: 'List View',
+    type: 'listview',
+  },
+  {
+    id: 7,
+    title: 'Menu',
+    type: 'menu',
+  },
+  {
+    id: 8,
+    title: 'Dropdown',
+    type: 'dropdown',
+  },
+  {
+    id: 6,
+    title: 'Select',
+    type: 'select',
+  },
 ];
 export { ALLCOMPONENTS };
